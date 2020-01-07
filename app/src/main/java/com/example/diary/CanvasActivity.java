9ea18c087bb3c1ebff8ebac71fafaa27f613f441
ChaMinzi <ckalswl331@naver.com>
2020-01-07 23:55:49 +0900

    Fix ColorPickerDialog Error

--- app/src/main/java/com/example/diary/CanvasActivity.java
+++ app/src/main/java/com/example/diary/CanvasActivity.java
@@ -48,7 +48,11 @@ public class CanvasActivity extends AppCompatActivity {
         Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
+        // if you want to fix colorWheelView.getView() Refer to "Fix ColorPickerDialog" in sourcetree
         final ColorWheelView colorWheelView = new ColorWheelView(this);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View view2 = inflater.inflate(R.layout.dialog_colorpicker, null, false);
+        ((FrameLayout)view2.findViewById(R.id.dialog_colorpicker)).addView(colorWheelView.getView());
 
         final Toolbar mDrawbar = (Toolbar)findViewById(R.id.drawbar);
         mDrawbar.inflateMenu(R.menu.drawbar_action);
@@ -63,60 +67,6 @@ public class CanvasActivity extends AppCompatActivity {
                     case R.id.action_eraser:
                         return true;
                     case R.id.action_colorpalette:
-                        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//                        View view = inflater.inflate(R.layout.dialog_colorwheel, null, false);
-//
-//                        ColorPicker picker = (ColorPicker) view.findViewById(R.id.colorpicker);
-//                        SVBar svBar = (SVBar) view.findViewById(R.id.svbar);
-//                        OpacityBar opacityBar = (OpacityBar) view.findViewById(R.id.opacitybar);
-//                        SaturationBar saturationBar = (SaturationBar) view.findViewById(R.id.saturationbar);
-//                        ValueBar valueBar = (ValueBar) view.findViewById(R.id.valuebar);
-//
-//                        picker.addSVBar(svBar);
-//                        picker.addOpacityBar(opacityBar);
-//                        picker.addSaturationBar(saturationBar);
-//                        picker.addValueBar(valueBar);
-//
-////To get the color
-//                        picker.getColor();
-//
-////To set the old selected color u can do it like this
-//                        picker.setOldCenterColor(picker.getColor());
-//// adds listener to the colorpicker which is implemented
-////in the activity
-//                        picker.setOnColorChangedListener(new ColorPicker.OnColorChangedListener() {
-//                            @Override
-//                            public void onColorChanged(int color) {
-//                                GlobalValue.get_instance().setColor(color);
-//                            }
-//                        });
-//
-////to turn of showing the old color
-//                        picker.setShowOldCenterColor(false);
-//
-////adding onChangeListeners to bars
-//                        opacityBar.setOnOpacityChangedListener(new OpacityBar.OnOpacityChangedListener() {
-//                            @Override
-//                            public void onOpacityChanged(int opacity) {
-//
-//                            }
-//                        });
-//                        valueBar.setOnValueChangedListener(new ValueBar.OnValueChangedListener() {
-//                            @Override
-//                            public void onValueChanged(int value) {
-//
-//                            }
-//                        });
-//                        saturationBar.setOnSaturationChangedListener(new SaturationBar.OnSaturationChangedListener() {
-//                            @Override
-//                            public void onSaturationChanged(int saturation) {
-//
-//                            }
-//                        });
-
-                        View view2 = inflater.inflate(R.layout.dialog_colorpicker, null, false);
-                        ((FrameLayout)view2.findViewById(R.id.dialog_colorpicker)).addView(colorWheelView.getView());
-
                         (new CustomDialog(mDrawbar, view2)).show();
                         //colorPickerDialog.show(getSupportFragmentManager()," tag");
                         return true;
